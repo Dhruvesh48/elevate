b5e6f874bf0228c627d7a64ce4ed13adb805dc4a
Dhruvesh48 <103595215+Dhruvesh48@users.noreply.github.com>
2024-12-12 05:12:46 +0000

    updated model for products and added views and template

--- products/models.py
+++ products/models.py
@@ -5,12 +5,15 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
+    sku = models.CharField(max_length=100, unique=True, default='SKU-DEFAULT')
     category = models.CharField(max_length=100, choices=[
         ('equipment', 'Equipment'),
         ('nutrition', 'Nutrition'),
         ('merchandise', 'Merchandise'),
     ])
+    rating = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def product_page(request):
+    """ A view to show all products"""
+    
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'products/product_page.html', context)
