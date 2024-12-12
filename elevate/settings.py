a444f083083abf550efbef301c44fd316b9f0bca
Dhruvesh48 <103595215+Dhruvesh48@users.noreply.github.com>
2024-12-12 04:17:54 +0000

    added home app views and templates, added custom css and base template

--- elevate/settings.py
+++ elevate/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 
 #ALLOWED_HOSTS = ['.herokuapp.com']
 
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    "user",
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +65,10 @@ ROOT_URLCONF = "elevate.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -146,7 +149,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- user/models.py
+++ user/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date, timedelta
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    fitness_goal = models.TextField(null=True, blank=True)
+    dietary_preference = models.TextField(null=True, blank=True)
+    subscription_status = models.CharField(
+        max_length=20,
+        choices=[('ACTIVE', 'Active'), ('INACTIVE', 'Inactive')],
+        default='INACTIVE'
+    )
+    renewal_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+    def save(self, *args, **kwargs):
+        if self.subscription_status == 'ACTIVE' and not self.renewal_date:
+            self.renewal_date = date.today() + timedelta(days=30)
+        super().save(*args, **kwargs)
