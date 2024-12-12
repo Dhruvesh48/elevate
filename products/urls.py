f66207698743345b1cb17cf7c6aecabc7c77d0bb
Dhruvesh48 <103595215+Dhruvesh48@users.noreply.github.com>
2024-12-12 07:20:50 +0000

    added product detail page

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.product_page, name='product_page'),
+    path('<int:pk>/', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -13,3 +13,14 @@ def product_page(request):
     }
 
     return render(request, 'products/product_page.html', context)
+
+def product_detail(request, pk):
+    """ A view to show individual product details """
+     
+    product = get_object_or_404(Product, pk=pk)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'products/product_detail.html', context)
