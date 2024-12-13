5a81e78f4abbe422422f594f94a5f2dbb808a51f
Dhruvesh48 <103595215+Dhruvesh48@users.noreply.github.com>
2024-12-13 17:11:50 +0000

    added update bag view

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add_to_bag/<int:item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('update_bag_item/<int:item_id>/', views.update_bag_item, name='update_bag_item'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from products.models import Product
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def view_bag(request):
@@ -10,8 +9,7 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-    
-    product = get_object_or_404(Product, pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -36,3 +34,32 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+def update_bag_item(request, item_id):
+    """Update the quantity of an item in the bag."""
+
+    if request.method == 'POST':
+        quantity = int(request.POST.get('quantity'))
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            if item_id in list(bag.keys()):
+                if size in bag[item_id]['items_by_size'].keys():
+                    bag[item_id]['items_by_size'][size] = quantity
+                else:
+                    bag[item_id]['items_by_size'][size] = quantity
+            else:
+                bag[item_id] = {'items_by_size': {size: quantity}}
+        else:
+            if item_id in list(bag.keys()):
+                bag[item_id] = quantity
+            else:
+                bag[item_id] = quantity
+        
+        request.session['bag'] = bag
+        return redirect('view_bag')
+
+    return redirect('view_bag')
